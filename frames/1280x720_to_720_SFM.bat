7e527b8595f3a5a2ca4fe441b85b6b6427f26b10
unknown <emmdouge@gmail.com>
2018-09-05 18:01:37 -0400

    specific folders

--- frames/1280x720_to_720_SFM.bat
+++ frames/1280x720_to_720_SFM.bat
@@ -1,5 +1,5 @@
-mkdir %1_sfm_frames
-ffmpeg -i %1 -r 12 -y %1_sfm_frames\input.mp4
-ffmpeg -i %1_sfm_frames\input.mp4 -filter:v scale=1280:720,crop=720:720:279:0 %1_sfm_frames\output.mp4
-ffmpeg -i %1_sfm_frames\output.mp4 -vf fps=24,scale=720:720,select=1 -vsync 0 %1_sfm_frames\out%%d.png
+mkdir %1_720_sfm_frames
+ffmpeg -i %1 -r 12 -y %1_720_sfm_frames\input.mp4
+ffmpeg -i %1_720_sfm_frames\input.mp4 -filter:v scale=1280:720,crop=720:720:279:0 %1_720_sfm_frames\output.mp4
+ffmpeg -i %1_720_sfm_frames\output.mp4 -vf fps=24,scale=720:720,select=1 -vsync 0 %1_720_sfm_frames\out%%d.png
 pause
