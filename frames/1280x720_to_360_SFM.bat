5c64550c48ab5b752ef9c55ed3c502e75c977ed0
unknown <emmdouge@gmail.com>
2018-09-05 17:55:55 -0400

    projection seed

--- frames/1280x720_to_360_SFM.bat
+++ frames/1280x720_to_360_SFM.bat
@@ -1,4 +1,5 @@
 mkdir %1_sfm_frames
 ffmpeg -i %1 -r 12 -y %1_sfm_frames\input.mp4
 ffmpeg -i %1_sfm_frames\input.mp4 -filter:v scale=1280:720,crop=720:720:279:0 %1_sfm_frames\output.mp4
+ffmpeg -i %1_sfm_frames\output.mp4 -vf fps=24,scale=360:360,select=1 -vsync 0 %1_sfm_frames\out%%d.png
 pause
