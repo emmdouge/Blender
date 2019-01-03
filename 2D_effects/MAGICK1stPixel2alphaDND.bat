fc299e6542d57b9bc7b1683593f1068b65570bc3
unknown <emmdouge@gmail.com>
2019-01-02 19:08:05 -0500

    complex shading

--- 2D_effects/MAGICK1stPixel2alphaDND.bat
+++ 2D_effects/MAGICK1stPixel2alphaDND.bat
@@ -1,8 +1,11 @@
-magick convert %1  -crop 1x1+1+1  txt:- 
+:again
 FOR /F "tokens=*" %%g IN ('magick convert %1  -crop 1x1+1+1  txt:-') do (SET VAR=%%g)
 for /F "tokens=3 delims= " %%a in ("%VAR%") do (
    SET VAR=%%a
 )
 echo:%VAR%
 magick convert %1 -fuzz 40%% -fill none -opaque %VAR% %1
+shift
+if “%~1” == “” goto:eof
+goto:again
 pause
