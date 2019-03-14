8a3a724905fcda04bd533cd508bf54c209d6104c
unknown <emmdouge@gmail.com>
2019-03-13 21:22:38 -0400

    more addons and scripts

--- frames/On2s.bat
+++ frames/On2s.bat
@@ -1,5 +1,6 @@
 mkdir %1_frames
 ffmpeg -i %1 -r 12 -y %1_frames\output.mp4
-ffmpeg -i %1_frames\output.mp4 -vf fps=24,scale=1280:720,select=1  -vsync 0 %1_frames\out%%d.png
+ffmpeg -i %1_frames\output.mp4 -vf fps=24,scale=1280:720,select=1  -vsync 0 %1_frames\in%%d.png
+REM FOR %%G IN (%1_frames\*.png) DO magick convert %%G -background black -alpha Remove %%G -compose Copy_Opacity -composite %%G
 REM for specific frames: ,select='between(t,37,45)' 
 pause
