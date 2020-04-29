dea779c9059ee7763aeb64753fe2ce0265283708
unknown <emmdouge@gmail.com>
2020-04-28 21:23:34 -0400

    bpm

--- frames/24fps_25pb_45bpm_12fpsVFR_2s.bat
+++ frames/24fps_25pb_45bpm_12fpsVFR_2s.bat
@@ -1,5 +1,5 @@
 mkdir %1_frames
 ffmpeg -i %1 -r 24 -vf "setpts=(4/1)*PTS" %1_frames\output.mp4
-ffmpeg -i %1_frames\output.mp4 -vf "select=not(mod(n\,6))" -vsync vfr %1_frames\out%%d.png
+ffmpeg -i %1_frames\output.mp4 -vf "select=not(mod(n\,8))" -vsync vfr %1_frames\out%%d.png
 REM for specific frames: ,select='between(t,37,45)' 
 pause
