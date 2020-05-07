c5198987e6b89dcebff7cb377e1d60db4bf9a387
unknown <emmdouge@gmail.com>
2020-05-06 23:34:20 -0400

    transparency

--- frames/24fps_25pb_120bpm_2fpsVFR_12s.bat
+++ frames/24fps_25pb_120bpm_2fpsVFR_12s.bat
@@ -1,7 +1,7 @@
 mkdir %1_frames
 ffmpeg -i %1 -r 24 -vf negate %1_frames\outputN.mp4 
 ffmpeg -i %1 -i %1_frames\outputN.mp4 -filter_complex "[0:v]scale=1280x720[bottom];[1:v]scale=1280x720[top];[bottom][top]overlay=enable='if(gt(n,0),not(mod(n\,12)),0)'" %1_frames\output12b.mp4
-ffmpeg -i %1_frames\output12b.mp4 -vf "select=not(mod(n\,12))" -vsync vfr %1_frames\out%%d.png
+ffmpeg -i %1_frames\output12b.mp4 -vf "select=not(mod(n\,12)),format=argb,colorchannelmixer=aa=0.40" -vsync vfr %1_frames\out%%d.png
 REM ffmpeg -i %1_frames\output12b.mp4 -r 24 -vf "setpts=(4/1)*PTS" %1_frames\output25pp.mp4
 REM for specific frames: ,select='between(t,37,45)' 
 REM to make first frame the original instead of negative overlay=enable='if(gt(n,0),not(mod(n\,64)),0)'
