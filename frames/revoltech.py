7300f3a386120c4fbb8a7e99fdec9c8c1e5f619e
unknown <emmdouge@gmail.com>
2020-05-13 21:28:30 -0400

    local rot

--- frames/revoltech.py
+++ frames/revoltech.py
@@ -1,5 +1,6 @@
 import bpy
 from math import pi, sqrt, degrees
+from mathutils import Matrix
 
 bl_info = {
     "name": "Revoltech Joint",
@@ -10,8 +11,15 @@ bl_info = {
     "description": "Move selected joint like a revoltech joint. "
                    "Locks rotation based on current rotation.",
     "warning": "",
-    "category": "Rigging"}
-    
+    "category": "Animation"}
+   
+def get_pose_bone_matrix(pose_bone):
+    local_matrix = pose_bone.matrix_channel.to_3x3()
+    if pose_bone.parent is None:
+        return local_matrix
+    else:
+        return pose_bone.parent.matrix_channel.to_3x3().inverted() @ local_matrix
+   
 class Revoltech(bpy.types.Panel):
     """Creates a Panel in the Object properties window"""
     bl_label = "Display Data"
@@ -28,13 +36,26 @@ class Revoltech(bpy.types.Panel):
         layout = self.layout
 
         bone = context.active_pose_bone
-        rot = bone.matrix.to_euler()
-        #bone.rotation_euler.rotate_axis("Z", radians(90))
+        armature = bone.id_data
+        
+        #debone = Matrix([[1,0,0,0],[0,0,-1,0], [0,1,0,0],[0,0,0,1]]) #because bones are wacky
+        #m = armature.matrix_world @ bone.matrix @ debone
+
+        q = get_pose_bone_matrix(bone).to_quaternion()
 
         row = layout.row()
         row.label(text="Active object is: " + bone.name)
         row = layout.row()
-        row.label(text="bone position z is %.2f" % rot.z)
+        row.label(text="Parent object is: " + bone.parent.name)
+        row = layout.row()
+        row.label(text="x is %.2f" % q[3])
+        row.label(text="y is %.2f" % q[1])
+        row.label(text="z is %.2f" % q[2])
+        #row = layout.row()
+        #row.label(text="xd is %.2f" % (abs(prot.x-rot.x)))
+        #row.label(text="yd is %.2f" % (abs(prot.y-rot.y)))
+        #row.label(text="zd is %.2f" % (abs(prot.z-rot.z)))
+        #row = layout.row()
         print ("bone position z is %.2f" % rot.z)
 
 def prop_redraw(scene):
